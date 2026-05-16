27ebc8371d008951dcf3168a316eee8cc82bcf42
Mayuri Shinde <mayuri.balasaheb.shinde@gmail.com>
2026-05-16 15:05:21 +0530

    saved local changes

---
+++ python-practice-repository/First_Program.py
@@ -0,0 +1,39 @@
+#Store:
+#your name
+#city
+#favorite color
+#and print them.
+
+
+name="Mayuri"S
+city="Pune"
+fav_color="Black"
+print("My name is :", name)
+print("My City Is " ,  city)
+print("My Favorite Color Is " ,  fav_color)
+
+
+#Take user input:
+
+# name
+# age
+
+# and print:
+
+name = input("Enter You Name.......")
+age=int(input("Enter Your Age......."))
+print("My NAme IS : " , name)
+print("My Age Is : " , age)
+
+
+# Create this:
+
+# Enter first number:
+# Enter second number:
+
+
+first_num=int(input("Enter First Number......."))
+second_num=int(input("Enter Second Number.......")) 
+print("First Number Is : " , first_num)
+print("Second Number Is : " , second_num)
+print("Sum Of Two Number Is : " , first_num + second_num)
