acef085f3f274c674d50f3d903d06dd057086b7a
mayurishinde11 <mayuri.balasaheb.shinde@gmail.com>
2026-05-17 14:21:33 +0530

    updated code

---
+++ python-practice-repository/Operators.py
@@ -0,0 +1,64 @@
+# Practice 1
+
+# Take 2 numbers from user and:
+
+# add
+# subtract
+# multiply
+# divide
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# print("Addition:", num1+num2)
+# print("Substraction=",num1-num2)
+# print("Multiplication: ",num1*num2)
+# print("Division: ", num1/num2)
+
+# Practice 2
+
+# Check if number is even or odd.
+
+# Hint:
+# Use %
+
+# num=float(input("Enter Number To Check If It Is Even Or Odd: "))
+# if num % 2==0:
+#     print("Number Is Even")
+# else:
+#     print("Number Is Odd")
+
+
+# Practice 3
+
+# Take user age and check:
+
+# eligible for voting or not
+
+
+# age=int(input("Enter Your Age To Check If You Are Eligible For Voting Or Not: "))
+# if age>=18:
+#     print("Yes, You Are Eligible For Voting")
+# else:
+#     print("Sorry, You Are Not Eligible For Voting")
+
+
+
+
+# 11. Mini Coding Task
+# Student Marks Calculator
+
+# Requirements:
+
+# Take 3 subject marks
+# Calculate total
+# Calculate average
+# Print results
+
+
+subject1=float(input("Enter Marks For Subject 1: "))
+subject2=float(input("Enter Marks For Subject 2: "))  
+subject3=float(input("Enter Marks For Subject 3: "))
+Total_Marks= subject1+subject2+subject3
+Average_Of_Marks=Total_Marks/3
+print("Total Marks: ", Total_Marks)
+print("Average Of Marks: ", Average_Of_Marks)
