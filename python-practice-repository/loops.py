ea01a5fb1650637644b8d00f5108b2f1a9e2f898
mayurishinde11 <mayuri.balasaheb.shinde@gmail.com>
2026-05-19 14:50:08 +0530

    updated code

---
+++ python-practice-repository/loops.py
@@ -0,0 +1,47 @@
+# Practice 1
+
+# Print numbers:
+# 1 to 10
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+# n=1
+# while n <=10:
+#     print(n)
+#     n=n+1    # without this loop never stop and it will print 1 infinite times because n is always 1 and it is less than 10 so loop called as infonity loop will never stop. So we have to increase n by 1 in each iteration to make it stop when n becomes 11
+
+
+# Practice 2
+
+# Print even numbers:
+# 1 to 20
+
+# Hint:
+# Use %
+
+# for i in range(1,21):
+#     if i %2==0:
+#         print(i)
+
+
+# n=1
+# while n<=20:
+#     if n % 2 != 1:
+#         print(n) 
+#     n=n+1
+
+# Practice 3
+
+# Print multiplication table of 5.
+
+# for i in range(1,11):
+#     print(5*i)
+
+
+n=1
+while n <=10:
+    print(5*n)
+    n=n+1
